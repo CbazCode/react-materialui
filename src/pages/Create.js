e4f46c91bbf483a7b1743c5ae3139e6da50f7b73
Sebastian Asis <sasisromero10@gmail.com>
2021-03-31 02:31:56 -0500

    :dart: IMPROVE: app-bar and avatars

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import {makeStyles} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles({
-  btn :{
-    backgroundColor: 'green',
-    '&:hover':{
-      backgroundColor: 'blue'
-    }
-    
+  field:{
+    marginTop:20,
+    marginBottom:20,
+    display: 'block',
+
   },
+  labelRadio:{
+    marginBottom:30
+  }
 });
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory()
+  const [title, setTitle] = useState('');
+  const [details, setDetails] = useState('');
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+  const [category, setCategory] = useState('money')
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      setTitleError(false);
+      setDetailsError(false);
+      if(!title || !details){
+        if(!title) setTitleError(true)
+        if(!details) setDetailsError(true)
+        return
+      }
+      fetch('http://localhost:3001/notes',{
+        method: 'POST',
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({ title, details, category })
+      }).then(() => history.push('/'));
+  }
+
+
   return (
     <Container>
       <Typography
@@ -30,16 +63,54 @@ export default function Create() {
       >
         Create a New Note
       </Typography>
-      <Button
-          onClick={()=> console.log('click')}
-          type="submit"
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField 
+          onChange={(e)=> setTitle(e.target.value)}
+          label="Note title"
           color="secondary"
-          variant="contained"
-          // startIcon={<SendIcon/>}
-          endIcon={<KeyboardArrowRightIcon/>}
-        >
-          Submit
-        </Button>
+          variant="outlined"
+          fullWidth
+          required
+          className={classes.field}
+          error={titleError}
+        />
+        <TextField 
+          onChange={(e)=> setDetails(e.target.value)}
+          label="Details"
+          color="secondary"
+          variant="outlined"
+          fullWidth
+          required
+          className={classes.field}
+          multiline
+          rows={4}
+          error={detailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel className={classes.labelRadio} >Note category</FormLabel>
+          <RadioGroup value= {category} onChange={(e)=> setCategory(e.target.value)}>
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="Todos" />
+            <FormControlLabel value="reminders" control={<Radio />} label="Reminders" />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+
+        <Button
+            
+            type="submit"
+            color="secondary"
+            variant="contained"
+            // startIcon={<SendIcon/>}
+            endIcon={<KeyboardArrowRightIcon/>}
+          >
+            Submit
+          </Button>
+        
+
+      </form>
+
     </Container>
   )
 }
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useFetch } from '../hooks/useFetch'
+import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import NoteCard from '../components/NoteCard'
 
 export default function Notes() {
+
+  const {data, loading} = useFetch('http://localhost:3001/notes');
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    setNotes(data);
+  }, [data])
+
+  const handleDelete = async (id) => {
+    await fetch('http://localhost:3001/notes/' + id, {
+      method: 'DELETE'
+    })
+    const newNotes = notes.filter(note => note.id != id)
+    setNotes(newNotes)
+  }
+
   return (
-    <div>
-      Notes page
-    </div>
+    <Container>
+      {
+        loading ?
+        <h1>Loading...</h1> :
+        <Grid container spacing={3}>
+          {
+            notes?.map(note => {
+              return (
+              <Grid item xs={12} md={6} lg={4} key={note.id}>
+                <NoteCard note={note} handleDelete={handleDelete} />
+              </Grid>)
+            })
+          }
+        </Grid>
+      }
+
+    </Container>
   )
 }
