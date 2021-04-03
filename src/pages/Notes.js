c5336f0cd8b8ae4156c24ba451b0132e334a6d6e
Sebastian Asis <sasisromero10@gmail.com>
2021-04-02 23:04:32 -0500

    :dart: IMPROVE: react-mansory-css

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -4,13 +4,14 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import NoteCard from '../components/NoteCard'
+import Masonry from 'react-masonry-css'
 
 export default function Notes() {
 
   const {data, loading} = useFetch('http://localhost:3001/notes');
 
   const [notes, setNotes] = useState([]);
-
+  console.log(notes);
   useEffect(() => {
     setNotes(data);
   }, [data])
@@ -23,21 +24,30 @@ export default function Notes() {
     setNotes(newNotes)
   }
 
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1
+  }
+
   return (
     <Container>
       {
         loading ?
         <h1>Loading...</h1> :
-        <Grid container spacing={3}>
+        <Masonry
+          breakpointCols={breakpoints}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column">
           {
             notes?.map(note => {
               return (
-              <Grid item xs={12} md={6} lg={4} key={note.id}>
+              <div xs={12} md={6} lg={4} key={note.id}>
                 <NoteCard note={note} handleDelete={handleDelete} />
-              </Grid>)
+              </div>)
             })
           }
-        </Grid>
+          </Masonry>
       }
 
     </Container>
